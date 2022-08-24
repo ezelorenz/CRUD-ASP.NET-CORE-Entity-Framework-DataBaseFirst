7b0591203c5815d892215870f75e50eececbb253
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2022-08-24 11:07:40 -0300

    first commit

---
+++ CRUDentity/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUDentity.Models;
+using CRUDentity.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CRUDentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CRUDENTITYContext _DBcontext;
+        public HomeController(CRUDENTITYContext context)
+        {
+            _DBcontext = context;
+        }
+
+        public IActionResult Index()
+        {
+            //creacion lista empleados donde seran asignados cada uno de los que estén en la DB
+
+            List<Empleado> lista = _DBcontext.Empleados.Include(c => c.oCargo).ToList();
+            return View(lista);
+        }
+        [HttpGet] //Creacion de objeto empleado, vacio, donde lo único que va a tener 
+                  // es la lista desplagable del cargo de cada uno. Se puede ver en Value
+        public IActionResult Empleado_Detalle(int idEmpleado)
+        {
+            EmpleadoVM oEmpleadoVM = new EmpleadoVM()
+            {
+                oEmpleado = new Empleado(),
+                oListaCargo = _DBcontext.Cargos.Select(cargo => new SelectListItem()
+                {
+                    Text = cargo.Descripcion,
+                    Value = cargo.IdCargo.ToString()
+                }).ToList()
+            };
+           
+            if(idEmpleado != 0)
+            {
+                oEmpleadoVM.oEmpleado = _DBcontext.Empleados.Find(idEmpleado);
+            }
+
+            return View(oEmpleadoVM);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Empleado_Detalle(EmpleadoVM oEmpleadoVM)
+        {   //Si no hay empleado asignado, crear empleado nuevo
+            if (oEmpleadoVM.oEmpleado.IdEmpleado == 0)
+            {
+                _DBcontext.Empleados.Add(oEmpleadoVM.oEmpleado);
+            }
+            else
+            {
+                _DBcontext.Empleados.Update(oEmpleadoVM.oEmpleado);
+            }
+
+            //Guardar los cambios en la base de datos
+            _DBcontext.SaveChanges();
+
+            //Redirecciona hacia la lista de empleados
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet] 
+        public IActionResult Eliminar(int idEmpleado)
+        {
+            Empleado oEmpleado = _DBcontext.Empleados.Include(c => c.oCargo).Where(e => e.IdEmpleado == idEmpleado).FirstOrDefault();
+            return View(oEmpleado);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(Empleado oEmpleado)
+        {
+            _DBcontext.Empleados.Remove(oEmpleado);
+            _DBcontext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CRUDentity/Models/CRUDENTITYContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUDentity.Models
+{
+    public partial class CRUDENTITYContext : DbContext
+    {
+        public CRUDENTITYContext()
+        {
+        }
+
+        public CRUDENTITYContext(DbContextOptions<CRUDENTITYContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cargo> Cargos { get; set; } = null!;
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cargo>(entity =>
+            {
+                entity.HasKey(e => e.IdCargo)
+                    .HasName("PK__CARGO__6C98562567AB7F14");
+
+                entity.ToTable("CARGO");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpleado)
+                    .HasName("PK__EMPLEADO__CE6D8B9E7FDB28BB");
+
+                entity.ToTable("EMPLEADO");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NombreCompleto)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.oCargo)
+                    .WithMany(p => p.Empleados)
+                    .HasForeignKey(d => d.IdCargo)
+                    .HasConstraintName("FK__EMPLEADO__IdCarg__267ABA7A");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
